7e824162ed19716f2d023a5b91af3313b78d039d
JMiltner97 <jan.miltner@fau.de>
2024-01-28 17:34:17 +0100

    update

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -8,14 +8,13 @@ urlretrieve(data, "GTFS.zip")
 with zipfile.ZipFile("GTFS.zip", 'r') as zip:
             zip.extract("stops.txt", '.')
 df = pd.read_csv("stops.txt")
-df = df['zone_id'] == 2001
+df = df.filter(items=['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id'])
+df = df[df['zone_id'] == 2001]
 df['valid_coordinates'] = (
     (df['stop_lat'] >= -90) & (df['stop_lat'] <= 90) &
     (df['stop_lon'] >= -180) & (df['stop_lon'] <= 180)
 )
 df = df[df['valid_coordinates']]
-
-
 df = df.filter(items=['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id'])
 #print(df)
 engine = create_engine("sqlite:///gtfs.sqlite")
