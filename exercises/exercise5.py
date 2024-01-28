bb6a72db3aec02f0a2491be3b376c23081f72405
JMiltner97 <jan.miltner@fau.de>
2024-01-28 17:25:02 +0100

    new

---
+++ exercises/exercise5.py
@@ -0,0 +1,22 @@
+import zipfile
+import pandas as pd
+from urllib.request import urlretrieve
+from sqlalchemy import create_engine
+
+data = "https://gtfs.rhoenenergie-bus.de/GTFS.zip"
+urlretrieve(data, "GTFS.zip")
+with zipfile.ZipFile("GTFS.zip", 'r') as zip:
+            zip.extract("stops.txt", '.')
+df = pd.read_csv("stops.txt")
+df = df['zone_id'] == 2001
+df['valid_coordinates'] = (
+    (df['stop_lat'] >= -90) & (df['stop_lat'] <= 90) &
+    (df['stop_lon'] >= -180) & (df['stop_lon'] <= 180)
+)
+df = df[df['valid_coordinates']]
+
+
+df = df.filter(items=['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id'])
+#print(df)
+engine = create_engine("sqlite:///gtfs.sqlite")
+df.to_sql(name="stops", con=engine, if_exists="replace", index=False)
